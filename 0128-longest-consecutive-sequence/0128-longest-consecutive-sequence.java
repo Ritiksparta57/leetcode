334ad3b54c9472929241ce4adc331046eefd9781
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-07-02 20:13:25 +0530

    Time: 32 ms (37.49%), Space: 99.3 MB (5.08%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -6,8 +6,8 @@ class Solution {
         if(!st.contains(nums[i]))st.add(nums[i]);
        } 
         int len=0;
-        for(int num:st){
-            if(!st.contains(num-1)){
+        for(int num:st){//WE  just check in the set of al letters not the whole array;
+            if(!st.contains(num-1)){// if the num-1 is there inset then the num is not the starting point of sequence thus we will surely visit the num-1 and there we wil take the longest;
             int count=0;
             int current=num;
             while(st.contains(current)){
