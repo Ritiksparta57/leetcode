96fe249136b018b2a821cf3a4420f6eda1094681
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-01 22:56:21 +0530

    Time: 1 ms (99.88%), Space: 55.6 MB (46.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,16 +4,32 @@ class Solution {
         //tc->o(n);
         //sc->o(n);
         //approach hashmap->
-        int comp=n/2;
-        int max=nums[0];
-        Map<Integer,Integer> m=new HashMap<>();
-        for(int i=0;i<n;i++)m.put(nums[i],m.getOrDefault(nums[i],0)+1);
-        for(Map.Entry<Integer,Integer> m1:m.entrySet()){
-            int val=m1.getValue();
-            if(val>comp){
-                max=m1.getKey();
+        // int comp=n/2;
+        // int max=nums[0];
+        // Map<Integer,Integer> m=new HashMap<>();
+        // for(int i=0;i<n;i++)m.put(nums[i],m.getOrDefault(nums[i],0)+1);
+        // for(Map.Entry<Integer,Integer> m1:m.entrySet()){
+        //     int val=m1.getValue();
+        //     if(val>comp){
+        //         max=m1.getKey();
+        //     }
+        // }
+        // return max;
+        //tc->o(n);
+        //sc->o(1);
+        //algo name-> BOYRE MOORE VOTING ALGO;
+        int count=0;
+        int cand=0;
+        for(int i=0;i<n;i++){
+            if(count==0){
+               cand=nums[i];
+               count++;
+            }
+            else if(cand==nums[i])count++;
+            else{
+                count--;
             }
         }
-        return max;
+        return cand;
     }
 }
