8a8b8b7785bbb8cf3eb8666a8d80fad2caf3659e
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-02 12:20:13 +0530

    Time: 2 ms (99.78%), Space: 48.5 MB (66.79%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -5,14 +5,12 @@ class Solution {
         int h=1;
         while(l<n&&h<n){
             if(nums[l]%2!=0){
-                int temp=nums[h];
-                nums[h]=nums[l];
-                nums[l]=temp;
-                h+=2;
+                int temp=nums[l];
+                nums[l]=nums[h];
+                nums[h]=temp;
+                h+=2;//the odd no. is at odd index and not increasing l as we do not know whether the element swapped is even or odd therefore as we are sure of h we may say that go on increasing h until all index h is odd;
             }
-            else{
-                l+=2;
-            }
+            else l+=2;// else we know that l is even;
         }
         return nums;
     }
