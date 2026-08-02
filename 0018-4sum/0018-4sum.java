c5d2d9002820985a03b9e74d26f25fa541620c18
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-02 15:09:01 +0530

    Time: 27 ms (12.38%), Space: 47.6 MB (5.69%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -6,10 +6,10 @@ class Solution {
          // here we need to iterate over two loops that is one for every element and other for next element of i
          // then perform 3 sum on second loop;
          //we need to fix i and j and then perform 3 sum;
-         for(int i=0;i<n;i++){
+         for(int i=0;i<n-3;i++){
             //to ensure that the array does not have duplicate;
             if(i>0&&nums[i]==nums[i-1])continue;
-            for(int j=i+1;j<n;j++){
+            for(int j=i+1;j<n-2;j++){
                 //ensure that the array after thsi number does not have duplicate;
                 if(j>i+1&&nums[j]==nums[j-1])continue;
                 //do 3 sum now;
