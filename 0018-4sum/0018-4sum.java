1feed445ff2323adbc4c8d3a68c79e65608d3f90
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-02 15:08:10 +0530

    Time: 25 ms (13.3%), Space: 47.6 MB (5.69%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n=nums.length;
+         List<List<Integer>> l=new ArrayList<>();
+         // here we need to iterate over two loops that is one for every element and other for next element of i
+         // then perform 3 sum on second loop;
+         //we need to fix i and j and then perform 3 sum;
+         for(int i=0;i<n;i++){
+            //to ensure that the array does not have duplicate;
+            if(i>0&&nums[i]==nums[i-1])continue;
+            for(int j=i+1;j<n;j++){
+                //ensure that the array after thsi number does not have duplicate;
+                if(j>i+1&&nums[j]==nums[j-1])continue;
+                //do 3 sum now;
+                int le=j+1;
+                int h=n-1;
+                while(le<h){
+                    long sum=(long)nums[i]+nums[j]+nums[le]+nums[h];
+                    List<Integer> a=new ArrayList<>();
+                    if(sum==(long)target){
+                        a.add(nums[i]);
+                        a.add(nums[j]);
+                        a.add(nums[le]);
+                        a.add(nums[h]);
+                        l.add(a);
+                        le++;
+                        h--;
+                        while(le<h&&nums[le]==nums[le-1])le++;
+                        while(le<h&&nums[h]==nums[h+1])h--;
+                    }
+                    else if(sum<(long)target)le++;
+                    else h--;
+                }
+            }
+         }
+         return l;
+    }
+}
