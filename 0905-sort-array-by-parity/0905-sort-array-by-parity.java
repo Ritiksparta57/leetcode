e271fbb6fe18d23883943893098031f778e3bb83
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-02 11:48:40 +0530

    Time: 0 ms (100%), Space: 47 MB (14.69%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -3,15 +3,16 @@ class Solution {
         int n=nums.length;
         int l=0;
         int h=n-1;
-        int mid=l;
+        int mid=l;//only for traversal;
         while(mid<=h){
             if(nums[mid]%2!=0){
-                int temp=nums[h];
-                nums[h]=nums[mid];
-                nums[mid]=temp;
+                int temp=nums[mid];
+                nums[mid]=nums[h];
+                nums[h]=temp;
                 h--;
+                // no effect on mid as we do not know whether it is even or odd;
             }
-            else mid++;
+            else mid++;//the mid is even i.e. where it should be;
         }
         return nums;
     }
