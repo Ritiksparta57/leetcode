eb2c371cfc0ef835f25ea2e30a9685603d87bc77
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-06 00:25:40 +0530

    Time: 66 ms (67.31%), Space: 292.6 MB (32.69%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.java
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -2,7 +2,9 @@ class Solution {
     public List<Integer> remainingMethods(int n, int k, int[][] invoc) {
         int m=invoc.length;
         List<List<Integer>> l=new ArrayList<>();
-        for(int i=0;i<n;i++)l.add(new ArrayList<>());
+        for(int i=0;i<n;i++){
+            l.add(new ArrayList<>());
+        }
         for(int[] edge:invoc){
             int u=edge[0];
             int v=edge[1];
@@ -11,24 +13,20 @@ class Solution {
         boolean[] vis=new boolean[n];
         Arrays.fill(vis,false);
         dfs(l,vis,k);
-        //the condition says if there exists even a single edge or even single neigh of the unvisited node then the whole group becomes unsuspicious;
+        //if any single element is not visited i.e. not part of the group and if any edged node is already visited then the group will be free of suspicion;
         for(int i=0;i<n;i++){
-            if(vis[i]==false){
+            if(vis[i]==false){//node unvisited
                 for(int num:l.get(i)){
-                    if(vis[num]==true){
-                        //the node which is connected to unsuspicious node is found and thus whole group is now safe thus make everyone in the array false;
-                        for(int j=0;j<n;j++){
-                            vis[j]=false;
-                        }
-                    }
+                  if(vis[num]){//node is visited
+                     //thus a direct cconnection to an unsuspicious one frees all the group memebers;
+                     for(int j=0;j<n;j++)vis[j]=false;
+                  }
                 }
             }
         }
         List<Integer> ans=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(vis[i]==false){
-            ans.add(i);
-            }
+            if(vis[i]==false)ans.add(i);
         }
         return ans;
     }
