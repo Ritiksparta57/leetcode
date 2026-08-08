e6472372a5d7bffd6b2a51ee2f316dc75ac9b821
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-09 01:34:08 +0530

    Time: 2 ms (99.32%), Space: 44 MB (96.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,39 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int n=s.length();
-        String s1="";
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            if(Character.isLetterOrDigit(ch)){
-                s1+=Character.toLowerCase(ch);
-            }
-        }
-        n=s1.length();
+        // String s1="";
+        // for(int i=0;i<n;i++){
+        //     char ch=s.charAt(i);
+        //     if(Character.isLetterOrDigit(ch)){
+        //         s1+=Character.toLowerCase(ch);
+        //     }
+        // }
+        // n=s1.length();
+        // int i=0;
+        // int j=n-1;
+        // while(i<=j){
+        //     char ch=s1.charAt(i);
+        //     char c=s1.charAt(j);
+        //     if(ch!=c){
+        //       return false;
+        //     }
+        //     i++;
+        //     j--;
+        // }
+        // let us try the optimal one->
         int i=0;
         int j=n-1;
         while(i<=j){
-            char ch=s1.charAt(i);
-            char c=s1.charAt(j);
-            if(ch!=c){
-              return false;
+            //we will increment i or j (decrement) when we encounter a non-alphanumeric character;
+            //first for i and then j;
+            while(i<j&&!Character.isLetterOrDigit(s.charAt(i))){
+                i++;
             }
+            while(i<j&&!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            //now let us convert the character to lowercase and then compare
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j)))return false;
             i++;
             j--;
         }
