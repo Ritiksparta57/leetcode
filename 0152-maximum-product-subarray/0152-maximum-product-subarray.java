088684612a01da8361907cb68294491ff76fa268
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-09 02:20:22 +0530

    Time: 0 ms (100%), Space: 47.2 MB (54.17%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -11,14 +11,14 @@ class Solution {
                 pro=1;
         }
         int suf=1;
-        for (int i=n - 1;i>=0;i--) {
+        for (int i=n-1;i>=0;i--) {
             suf*=nums[i];
             suff[i]=suf;
             if (suf==0)suf=1;
         }
-        int max = Integer.MIN_VALUE;
+        int max=Integer.MIN_VALUE;
         for (int i=0;i<n;i++) {
-            max=Math.max(max, Math.max(pre[i], suff[i]));
+            max=Math.max(max,Math.max(pre[i],suff[i]));
         }
         return max;
     }
