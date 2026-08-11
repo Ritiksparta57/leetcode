2a77b04bf4f0280bc18922fbbc401c0e0613adc8
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-12 00:42:03 +0530

    Time: 32 ms (35.55%), Space: 95.8 MB (16.16%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -2,7 +2,9 @@ class Solution {
     public int longestConsecutive(int[] nums) {
        int n=nums.length;
        //we just need to care about that number from where the sequence will start;
-       //we will set up a set in which all no. will be stored and then we will search for the number which do not have number -1 in the set and that is where the sequnece starts from;
+       //we will set up a set in which all no. will be stored and then we will search for the number in set which do not have number-1 in the set and that is where the sequnece starts from;
+       //then we run a while loop to find the len for the sequnce that start from that number 
+       //that is done using while(st.contains(number+1));
        if(n==0)return 0;
        Set<Integer> st=new HashSet<>();
        for(int num:nums)st.add(num);
