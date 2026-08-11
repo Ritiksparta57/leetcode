c6f568216681cf49dea2c11e84db4a7f40570e79
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-11 19:45:12 +0530

    Time: 23 ms (93.71%), Space: 49.2 MB (12.23%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -6,12 +6,12 @@ class Solution {
         int sum=0;
         int count=0;
         for(int i=0;i<n;i++){
-           sum+=nums[i];
-           int remove=sum-k;
-           if(m.containsKey(remove)){
-            count+=m.get(remove);
-           }
-           m.put(sum,m.getOrDefault(sum,0)+1);
+            sum+=nums[i];
+            int rem=sum-k;
+            if(m.containsKey(rem)){
+                count+=m.get(rem);
+            }
+            m.put(sum,m.getOrDefault(sum,0)+1);
         }
         return count;
     }
