fee77742be7357792c3782f8f0b4b6125b3bc4db
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-14 00:26:29 +0530

    Time: 2 ms (29.93%), Space: 47.3 MB (57.97%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -4,6 +4,10 @@ class Solution {
        //in jump game 1 we needed to tell whether we can reach the end or not;
        //here it is already given that we will reach the end;
        //the only thing which we need to calculate is the no. of steps;
+       //after calculating each jump we have a reach (maximum) thus until i is less than max we iterate over the values in between i and max;
+       //if the reach of them is more than reach(of i which is proven to be greater than max) then we replace reach with r(calculated one)then after iterating till max we replace max with highest reach;
+       //also increment count as we have taken a jump and then check whether max has reached the end;
+       //if so then return count;
        if(n<=1)return 0;
        int count=0;
        int max=0;
