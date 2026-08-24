d5212e66ea5ce024844e5a1cc29527896c0331ff
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-24 14:30:42 +0530

    Time: 3 ms (57.7%), Space: 49.2 MB (9.73%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,31 +1,27 @@
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        //we need to reconstruct afterr making the dp array or we do backtracking;
-        //it is asking for giving it all the combinations therefore i am gonna use backtracking;
         int n=s.length();
-        List<String> l=new ArrayList<>();
         Set<String> st=new HashSet<>();
+        for(String c:wordDict)st.add(c);
+        List<String> l=new ArrayList<>();
         String sb="";
-        for(String s1:wordDict)st.add(s1);
         dfs(0,n,s,st,sb,l);
         return l;
     }
     public void dfs(int i,int n,String s,Set<String> st,String sb,List<String> l){
-        //base case is whne we reach n;
-        //see what happens is when i recurse after seeing that the substring is in the set then we are moving i to j+1 but if the string has a part which is not in set then at that point i will not move forward and will never go to bc i==n thus the string will never get added to list as the base case in fulfilled;
-        //in word break 1 also when we were checking if substring belongs to stet also the next substring from j+1 should also belong and if above problem occurs then j never reaches bc thus again returning false at the end;
+        //whenever the i reaches n then only the strign is added  to l;
+        //now when we check for dfs(j+1) and if there is no substring that belongs to set then i never reaches n thus the string never gets added to l;
         if(i==n){
             l.add(sb.trim());
             return;
         }
-        if(i>n)return;
-        //here we add the substrings to the string sb;
+        String org=sb;//for backtracking purpose;
+        //making of substring and checking its existence in set;
         for(int j=i;j<n;j++){
-            String org=sb;
             String sub=s.substring(i,j+1);
             if(st.contains(sub)){
                 sb+=sub+' ';
-                dfs(j+1,n,s,st,sb,l);//running for next one taking sb as string;
+                dfs(j+1,n,s,st,sb,l);
                 sb=org;
             }
         }
