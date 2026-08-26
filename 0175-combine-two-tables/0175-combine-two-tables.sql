d8f1324e081400ea0729b918a787fd69db4b2994
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-26 14:58:52 +0530

    Time: 403 ms (86.74%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-select firstName,lastName,city,state from person left join address on person.personId=address.personId;
+#we want the first name,last name,city and state of a person which has its information stored in two tables;
+#therefore join will be applied;
+#And the basis or the column on which they will be joined will be personId;
+#Also we want the  null to be represented thus no need to worry about null check;
+#One More important thing that is since we want all from the first table then we use left join as every category from person is used;
+Select person.firstName,person.lastName,address.city,address.state
+FROM Person Left Join Address ON person.personId=address.personId
