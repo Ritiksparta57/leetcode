7106ed486aaf4098c81aba9ce40d145f6d33eb20
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-26 22:20:28 +0530

    Time: 431 ms (43.58%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,2 +1,7 @@
 # Write your MySQL query statement below
-select e.name as Employee from employee e inner join employee m on e.managerId=m.id where(e.salary>m.salary);
+#we need to join the same tables but on different basis that is on the basis of managerid
+select e1.name as Employee
+from Employee e1
+join Employee e2 
+on e1.managerId=e2.id
+where e1.salary>e2.salary
