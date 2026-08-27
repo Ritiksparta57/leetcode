dde89781bf392a0de9e7f209d2adcc42ec24f3bb
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-27 23:56:33 +0530

    Time: 727 ms (21.28%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,14 +1,7 @@
 # Write your MySQL query statement below
-#What i am thinking is using a right join and then joining the first table to the second using the id and customer id and then using where clause and notin clause;
--- SELECT c.name AS Customers
--- FROM CUSTOMERS c
--- WHERE id NOT IN(
---     SELECT c.id
---     FROM customers c RIGHT JOIN orders o ON c.id=o.customerId
--- )
-#even better solution is->
-#selecting those customers after applying left join whose value is null
-SELECT c.name as Customers
-from customers c
-Left Join orders o on c.id=o.customerId
-where o.customerId is null;
+#left join on the  table using cid and id and after after join those who got null in there cid are the ones with no order;
+select c1.name AS Customers
+FROM customers c1 
+left join orders c2
+on c1.id=c2.customerId
+where c2.customerId is null
