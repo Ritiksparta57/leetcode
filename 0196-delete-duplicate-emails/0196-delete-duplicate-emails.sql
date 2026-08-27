18996ca7a802b18f710deda0ac05e96cd89e3dd5
Ritik kr pandey <ritikkrpandey3145@gmail.com>
2026-08-27 23:47:24 +0530

    Time: 934 ms (45.23%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
 delete p1
-from Person p1
-join Person p2
+from person p1
+join person p2
 on p1.email=p2.email
-and p1.id>p2.id
-#so here this query deletes the record p1 from person which in case has same email as p2 which is said by the join clause as join is performed only by matching subjects/elements;
-#also if p1 is having greater id than p2 then p1 gets deleted;
+where p1.id>p2.id
